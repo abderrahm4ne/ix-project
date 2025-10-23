d32564f564c102780c4d307148d25a97a1b0b90a
abderrahm4ne <abderrahmaneita@gmail.com>
2025-10-23 22:51:55 +0100

    f

---
+++ backend/src/db/mongo.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const mongoConnection = mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: process.env.DB_NAME
+}).then(() => {
+    console.log({message: 'Connected to MongoDB'});
+}).catch((error) => {
+    console.error({message: 'Error connecting to MongoDB', error: error})
+})
+
+export default mongoConnection;
