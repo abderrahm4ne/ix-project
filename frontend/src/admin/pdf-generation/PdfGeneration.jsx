f041f29f4f164191406352c9993b999354a5c691
abderrahm4ne <abderrahmaneita@gmail.com>
2026-02-17 00:19:40 +0100

    adding branch

--- frontend/src/admin/pdf-generation/PdfGeneration.jsx
+++ frontend/src/admin/pdf-generation/PdfGeneration.jsx
@@ -15,6 +15,8 @@ export default function PdfGeneration() {
   const firstPageItems = allItems.slice(0, 28);
   const secondPageItems = allItems.slice(28);
 
+
+  // testing branching with adding new branch
   const formatDate = (dateString) => {
     if (!dateString) return '';
     return new Date(dateString).toLocaleDateString('en-US', {
