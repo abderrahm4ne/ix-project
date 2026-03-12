e146b90f607e54a6b224f85c6c0d310c84cf2072
abderrahm4ne <abderrahmaneita@gmail.com>
2026-03-12 14:22:23 +0100

    fixing form

--- frontend/src/admin/AdminDashboard.jsx
+++ frontend/src/admin/AdminDashboard.jsx
@@ -5,7 +5,7 @@ export default function AdminDashboard(){
     const navigate = useNavigate();
     return(
         <div className="flex flex-col ">
-            <div className="flex flex-row bg-orange-700 justify-center items-center py-4 gap-4 md:gap-5 lg:gap-8">
+            <div className="flex flex-row bg-orange-700 justify-center items-center py-4 gap-4 md:gap-5 lg:gap-8`">
                 <h1 onClick={() => navigate("/secret/admin/dashboard")} className="btn lg:text-3xl md:text-2xl text-xl text-center">Admin dashboard</h1>
                 <NavLink to="/secret/admin/dashboard/admin-contacts" className="btn lg:text-3xl md:text-2xl text-xl text-center">Contacts and orders</NavLink>
 
